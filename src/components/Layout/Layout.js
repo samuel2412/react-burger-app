8a24d21dead41d2a505638007b358133e5c4875e
Samuel Alves <samuel241296@hotmail.com>
2019-12-07 11:20:11 -0300

    BurgerIngredient concluido

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Aux from '../../Aux copy'
+
+const layout = (props) =>(
+    <Aux>
+        <div>Toolbar, SideDrawer, BackDrop</div>
+        <main>
+            {props.children}
+        </main>
+    </Aux>
+
+)
+
+export default layout;
