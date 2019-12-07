34b8fd629f4037e3ba3d8cd1c9b2f0b600f602e5
Samuel Alves <samuel241296@hotmail.com>
2019-12-07 19:32:35 -0300

    ingredientes prepados para serem exbidos dinamicamente, botoes para controlar ingredientes adicionados ( ainda nao funcionais )

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,13 +3,23 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import './Burger.css'
 
 const Burger = (props) => {
+
+    let transformedIndredients = Object.keys( props.ingredients ).map(igKey =>{
+        return [...Array( props.ingredients[igKey] )].map(( _,index) => {
+            return <BurgerIngredient key={igKey + index} type={igKey}/>;
+        })
+    }).reduce((arr,el) => {
+        return arr.concat(el);
+    },[]);
+   
+    if(transformedIndredients.length === 0){
+        transformedIndredients = <p>Please start adding ingredients!</p>
+    }
+
     return (
         <div className={"Burger"}>
             <BurgerIngredient type={'bread-top'} />
-            <BurgerIngredient type={'bacon'} />
-            <BurgerIngredient type={'bacon'} />
-            <BurgerIngredient type={'cheese'} />
-            <BurgerIngredient type={'meat'} />
+                { transformedIndredients}
             <BurgerIngredient type={'bread-bottom'} />
         </div>
 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
 import Aux from '../../Aux copy'
 import Burger from '../../components/Burger/Burger'
+import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 
 export default class BurgerBuilder extends Component {
+    state = {
+        ingredients:{
+            salad:0,
+            bacon:0,
+            cheese:0,
+            meat:0
+        }
+    }
+
 
     render() {
         return (
             <Aux>
-                <Burger />
+                <Burger ingredients={this.state.ingredients}/>
+                <BuildControls />
             </Aux>
         );
     }
