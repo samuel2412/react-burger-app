86b7f195454981570eb963189e6f6b2781e50557
Samuel Alves <samuel241296@hotmail.com>
2019-12-09 00:27:09 -0300

    navigation items implementado

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Toolbar.css'
 import Logo from '../../Logo/Logo'
+import NavgationItems from '../NavigationItems/NavigationItems'
 const toolbar = (props) =>(
     <header className={'Toolbar'}>
         <div>
@@ -8,7 +9,7 @@ const toolbar = (props) =>(
         </div>
         <Logo/>
         <nav>
-            ...
+            <NavgationItems/>
         </nav>
     </header>
 )
