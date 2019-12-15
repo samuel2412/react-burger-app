f12fab81e0e9b1b2bb8e1d542cf924c16d46f437
Samuel Alves <samuel241296@hotmail.com>
2019-12-15 18:26:55 -0300

    redirect na pagina de checkout para evitar erro causado por entrar diretamente nela sem passar pela pagina do burgerBuilder

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,37 +1,40 @@
 import React, { Component } from 'react'
-import {Route} from 'react-router-dom';
-import {connect} from 'react-redux';
+import { Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 import ContactData from './ContactData/ContactData'
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 
 class Checkout extends Component {
 
 
-    checkoutContinueHandler = () =>{
-       this.props.history.replace('/checkout/contact-data');
+    checkoutContinueHandler = () => {
+        this.props.history.replace('/checkout/contact-data');
     }
 
-    checkoutCancelledHandler = () =>{
+    checkoutCancelledHandler = () => {
         this.props.history.goBack();
     }
 
     render() {
-
-        return (
-            <div>
-                <CheckoutSummary 
-                ingredients={this.props.ings}
-                continueHandler={this.checkoutContinueHandler}
-                cancelHandler={this.checkoutCancelledHandler}
-                />
-                <Route path={this.props.match.path + '/contact-data'} component={ ContactData }/>
-            </div>
-        );
+        let summary = <Redirect to='/' />
+        if (this.props.ings) {
+            summary = (
+                <div>
+                    <CheckoutSummary
+                        ingredients={this.props.ings}
+                        continueHandler={this.checkoutContinueHandler}
+                        cancelHandler={this.checkoutCancelledHandler}
+                    />
+                    <Route path={this.props.match.path + '/contact-data'} component={ContactData} />
+                </div>
+            );
+        }
+        return summary;
     }
 }
 
-const mapStateProps = state =>  {
-    return{
+const mapStateProps = state => {
+    return {
         ings: state.ingredients
     };
 }
