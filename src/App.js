c05174ebfbc3f91018f7f1247555d2d161d94cb7
Samuel Alves <samuel241296@hotmail.com>
2019-12-30 18:01:38 -0300

    link para logout adicionado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
+
 import Layout from './components/Layout/Layout';
 import BugerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
+import Logout from './containers/Auth/Logout/Logout';
 
 export default class App extends Component {
   render() {
@@ -17,6 +19,7 @@ export default class App extends Component {
             <Route exact path='/' component={BugerBuilder} />
             <Route path='/orders' component={Orders} />
             <Route path='/auth' component={Auth} />
+            <Route path='/logout' component={Logout} />
             <Route path='/checkout' component={Checkout} />
           </Switch>
 
