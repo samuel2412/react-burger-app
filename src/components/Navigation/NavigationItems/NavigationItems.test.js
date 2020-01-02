4313362f8b7a4bd54f67b3bade3a0954e85f0bdd
Samuel Alves <samuel241296@hotmail.com>
2020-01-02 14:38:42 -0300

    testes automaticos (jest+enzyme) do component NavigationItems

---
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import NavigationItems from './NavigationItems';
+import NavigationItem from './NavigationItem/NavigationItem';
+
+configure({ adapter: new Adapter() });
+
+describe('<NavigationItems />', () => {
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(<NavigationItems />);
+    })
+
+    it('should render 2 <NavigationItems/> elements if not authenticated', () => {
+        expect(wrapper.find(NavigationItem)).toHaveLength(2);
+    });
+
+    it('should render 3 <NavigationItems/> elements if authenticated', () => {
+        wrapper.setProps({ isAuth: true })
+        expect(wrapper.find(NavigationItem)).toHaveLength(3);
+    });
+
+    it(`should contain <NavigationItem link={'/logout'}>Logout</NavigationItem> if authenticated`, () => {
+        wrapper.setProps({ isAuth: true })
+        expect(wrapper.contains(<NavigationItem link={'/logout'}>Logout</NavigationItem>)).toEqual(true);
+    });
+
+
+
+})
