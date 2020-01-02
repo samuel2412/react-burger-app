ade17a28e3502dc19f424506d428bd7ce63bb845
Samuel Alves <samuel241296@hotmail.com>
2020-01-02 12:13:23 -0300

    refatorações - reducers refatorados(usando função compatilhada de updateObject e switch case reduzido)

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -1,48 +1,52 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../../shared/utility';
 
-const initialState= {
-    tokenId:null,
-    userId:null,
-    error:null,
-    loading:false,
+const initialState = {
+    tokenId: null,
+    userId: null,
+    error: null,
+    loading: false,
     authRedirectPath: '/'
 };
- const reducer = (state=initialState,action) =>{
+const authStart = (state, action) => {
+    return updateObject(state, { error: null, loading: true });
+};
 
-    switch(action.type){
-        case(actionTypes.AUTH_START) :
-        return{
-            ...state,
-            loading:true,
-            error:null
-        }
-        case(actionTypes.AUTH_SUCCESS) :
-        return{
-            ...state,
-            userId: action.userId,
-            tokenId: action.tokenId,
-            loading: false
-        }
-        case(actionTypes.AUTH_FAIL) :
-        return{
-            ...state,
-            error: action.error,
-            loading: false
-        }
-        case(actionTypes.AUTH_LOGOUT) :
-        return{
-            ...state,
-            userId:null,
-            tokenId:null
-        }
-        case(actionTypes.AUTH_REDIRECT_PATH):
-        return{
-            ...state,
-            authRedirectPath: action.path
-        }
+const authSuccess = (state, action) => {
+    return updateObject(state, {
+        tokenId: action.tokenId,
+        userId: action.userId,
+        error: null,
+        loading: false
+    });
+};
+
+const authFail = (state, action) => {
+    return updateObject(state, {
+        error: action.error,
+        loading: false
+    });
+};
+
+const authLogout = (state, action) => {
+    return updateObject(state, { tokenId: null, userId: null });
+};
+
+const authRedirectPath = (state, action) => {
+    return updateObject(state, { authRedirectPath: action.path })
+}
+
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case actionTypes.AUTH_START: return authStart(state, action);
+        case actionTypes.AUTH_SUCCESS: return authSuccess(state, action);
+        case actionTypes.AUTH_FAIL: return authFail(state, action);
+        case actionTypes.AUTH_LOGOUT: return authLogout(state, action);
+        case actionTypes.AUTH_REDIRECT_PATH: return authRedirectPath(state, action);
         default: return state;
     }
 
- }
+}
 
- export default reducer;
+export default reducer;
--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../../shared/utility';
 
 const INGREDIENT_PRICES = {
     salad: 0.7,
@@ -13,51 +14,52 @@ const initialState = {
     error: false,
     building: false
 };
+const addIngredient = (state, action) => {
+    return updateObject(state, {
+        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName],
+        ingredients: {
+            ...state.ingredients,
+            [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+        },
+        building: true
+    });
+};
+const removeIngredient = (state, action) => {
+    return updateObject(state, {
+        totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName],
+        ingredients: {
+            ...state.ingredients,
+            [action.ingredientName]: state.ingredients[action.ingredientName] - 1
+        },
+        building: true
+    });
+};
+const setIngredients = (state, action) => {
+    return updateObject(state, {
+        ingredients: {
+            salad: action.ingredients.salad,
+            bacon: action.ingredients.bacon,
+            cheese: action.ingredients.cheese,
+            meat: action.ingredients.meat
+        },
+        totalPrice: 5,
+        error: false,
+        building: false
+    });
+};
+const fetchIngredientsFailed = (state, action) => {
+    return updateObject(state, {
+        error: true
+    });
+};
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case (actionTypes.ADD_INGREDIENT):
-            return {
-                ...state,
-                totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName],
-                ingredients:{
-                    ...state.ingredients,
-                    [action.ingredientName]: state.ingredients[action.ingredientName]+1
-                },
-                building: true
-
-            } 
-        case(actionTypes.REMOVE_INGREDIENT):
-        return{
-            ...state,
-            totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName],
-            ingredients:{
-                ...state.ingredients,
-                [action.ingredientName]: state.ingredients[action.ingredientName]-1
-            },
-            building: true
-        }
-        case(actionTypes.SET_INGREDIENTS):
-        return{
-            ...state,
-            ingredients: {
-                salad: action.ingredients.salad,
-                bacon: action.ingredients.bacon,
-                cheese: action.ingredients.cheese,
-                meat: action.ingredients.meat
-            },
-            totalPrice:5,
-            error: false,
-            building: false
-        }
-        case(actionTypes.FETCH_INGREDIENTS_FAILED):
-        return{
-            ...state,
-            error: true
-        }
-
-        default:
-            return state;
+        case (actionTypes.ADD_INGREDIENT): return addIngredient(state, action)
+        case (actionTypes.REMOVE_INGREDIENT): return removeIngredient(state, action)
+        case (actionTypes.SET_INGREDIENTS): return setIngredients(state, action)
+        case (actionTypes.FETCH_INGREDIENTS_FAILED): return fetchIngredientsFailed
+        default: return state;
     }
 };
 
